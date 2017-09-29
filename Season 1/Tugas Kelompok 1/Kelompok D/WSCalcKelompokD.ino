55c207bac3cdeb5c30c1eaf95fd7209228d8d3ae
GShay <gabrielleshay@yahoo.com>
2017-09-29 22:46:12 +0700

    Add files via upload

---
+++ Season 1/Tugas Kelompok 1/Kelompok D/WSCalcKelompokD.ino
@@ -0,0 +1,133 @@
+#include <Keypad.h>
+#include <Wire.h> 
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(0, 1, 2, 3, 4, 5);
+
+long first = 0;
+long second = 0;
+double total = 0;
+
+char customKey;
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char keys[ROWS][COLS] = {
+  {'1','4','7','C'},
+  {'2','5','8','0'},
+  {'3','6','9','='},
+  {'+','-','*','/'}
+};
+byte rowPins[ROWS] = {13,12,11,10}; 
+byte colPins[COLS] = {9,8,7,6}; 
+
+
+Keypad customKeypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS); 
+
+void setup()
+{
+lcd.begin(16, 2);              
+for(int i=0;i<=3;i++);
+  lcd.println("           WSCalc 2017 Calculator");
+  lcd.setCursor(0,1);
+  lcd.println("           Made by Kelompok D");
+  for (int positionCounter = 0; positionCounter < 33 ; positionCounter++) {
+    lcd.scrollDisplayLeft();
+    delay(200);
+  }
+  lcd.clear();
+delay(3000);
+lcd.clear();
+lcd.setCursor(0, 0);
+}
+
+void loop()
+{
+
+
+  customKey = customKeypad.getKey();
+  switch(customKey) 
+  {
+  case '0' ... '9': 
+    lcd.setCursor(0,0);
+    first = first * 10 + (customKey - '0');
+    lcd.print(first);
+    break;
+
+  case '+':
+    first = (total != 0 ? total : first);
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(0,1);
+    lcd.print("+");
+    second = SecondNumber(); 
+    total = first + second;
+    lcd.setCursor(0,3);
+    lcd.print(total);
+    first = 0, second = 0; 
+    break;
+
+  case '-':
+    first = (total != 0 ? total : first);
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(0,1);
+    lcd.print("-");
+    second = SecondNumber();
+    total = first - second;
+    lcd.setCursor(0,3);
+    lcd.print(total);
+    first = 0, second = 0;
+    break;
+
+  case '*':
+    first = (total != 0 ? total : first);
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(0,1);
+    lcd.print("*");
+    second = SecondNumber();
+    total = first * second;
+    lcd.setCursor(0,3);
+    lcd.print(total);
+    first = 0, second = 0;
+    break;
+
+  case '/':
+    first = (total != 0 ? total : first);
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(0,1);
+    lcd.print("/");
+    second = SecondNumber();
+    lcd.setCursor(0,3);
+
+    second == 0 ? lcd.print("Invalid") : total = (float)first / (float)second;
+
+    lcd.print(total);
+    first = 0, second = 0;
+    break;
+
+  case 'C':
+    total = 0;
+    lcd.clear();
+    break;
+  }
+}
+
+long SecondNumber()
+{
+  while( 1 )
+  {
+    customKey = customKeypad.getKey();
+    if(customKey >= '0' && customKey <= '9')
+    {
+      second = second * 10 + (customKey - '0');
+      lcd.setCursor(0,2);
+      lcd.print(second);
+    }
+
+    if(customKey == '=') break;  
+  }
+ return second; 
+}
